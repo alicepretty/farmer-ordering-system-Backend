35763287f7bc15ae615efe65b24c4e3e20ce5426
alicepretty <alicemuhorakeye15@gmail.com>
2024-06-03 17:40:30 +0200

    Fix redirect

--- src/utils/apiRedirect.ts
+++ src/utils/apiRedirect.ts
@@ -4,5 +4,12 @@ import express from "express";
 config();
 // const app_fe_url: string = process.env.APP_FE;
 export const redirectTohome = (req: express.Request, res: express.Response) => {
-//   res.status(301).redirect(app_fe_url);
+  //   res.status(301).redirect(app_fe_url);
+};
+
+export const redirectToDocumentation = (
+  req: express.Request,
+  res: express.Response
+) => {
+  res.redirect("https://documenter.getpostman.com/view/32403169/2sA3Qy58b4");
 };
--- src/utils/setRoutes.ts
+++ src/utils/setRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-// import { redirectTohome } from "./appredirects";
+import { redirectToDocumentation } from "./apiRedirect";
 import userRouter from "./../routes/user.routes";
 import landRouter from "./../routes/land.routes";
 import fertilizerRouter from "./../routes/fertilizer.routes";
@@ -7,13 +7,13 @@ import seedRouter from "./../routes/seed.routes";
 import orderRouter from "./../routes/order.routes";
 
 const setRoutes = (app: express.Application) => {
-//   app.get(["/", "/api"], redirectTohome);
+  app.get(["/", "/api"], redirectToDocumentation);
+
   app.use("/api/users", userRouter);
   app.use("/api/lands", landRouter);
   app.use("/api/seeds", seedRouter);
   app.use("/api/fertilizers", fertilizerRouter);
   app.use("/api/orders", orderRouter);
-
 };
 
 export default setRoutes;
